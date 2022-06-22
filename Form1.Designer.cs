033b2e8ed295a0672103faef238abc9f652b9709
wesze <wesze@DESKTOP-G9S4KOU>
2022-06-22 20:40:35 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(280, 156);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 23);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(280, 185);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 23);
+            this.btn_4.TabIndex = 1;
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(280, 214);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(75, 23);
+            this.btn_7.TabIndex = 2;
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(361, 156);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 23);
+            this.btn_2.TabIndex = 3;
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(361, 185);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(75, 23);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(361, 214);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(75, 23);
+            this.btn_8.TabIndex = 5;
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(442, 156);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 23);
+            this.btn_3.TabIndex = 6;
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(442, 185);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(75, 23);
+            this.btn_6.TabIndex = 7;
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(442, 214);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(75, 23);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int numberofclicks = 0;
+        public bool nyert = false;
+        public string gyoztesplayer;
+        public string xorcircle;
+        public void jatekvege()
+        {
+            DialogResult dialogResult = MessageBox.Show("\nSzeretnél új játékot kezdeni?", gyoztesplayer + " játékos nyert!", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                this.Close();
+            }
+        }
+        public void wincheck()
+        {
+            //sorok
+            if (btn_1.Text == btn_2.Text && btn_2.Text == btn_3.Text && btn_2.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_1.Text;
+                jatekvege();
+            }
+            else if (btn_4.Text == btn_5.Text && btn_5.Text == btn_6.Text && btn_5.Text != "")
+            {
+                nyert=true;
+                gyoztesplayer = btn_4.Text;
+                jatekvege();
+            }
+            else if (btn_7.Text == btn_8.Text && btn_8.Text == btn_9.Text && btn_8.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_7.Text;
+                jatekvege();
+            }
+            //oszlopok
+
+            if (btn_1.Text == btn_4.Text && btn_4.Text == btn_7.Text && btn_4.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_1.Text;
+                jatekvege();
+            }
+            else if (btn_2.Text == btn_5.Text && btn_5.Text == btn_8.Text && btn_5.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_2.Text;
+                jatekvege();
+            }
+            else if (btn_3.Text == btn_6.Text && btn_6.Text == btn_9.Text && btn_6.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_3.Text;
+                jatekvege();
+            }
+            //sréhen (tudod a / vagy a \)
+
+            if (btn_1.Text == btn_5.Text && btn_5.Text == btn_9.Text && btn_5.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_1.Text;
+                jatekvege();
+            }
+            else if (btn_3.Text == btn_5.Text && btn_5.Text == btn_7.Text && btn_5.Text != "")
+            {
+                nyert = true;
+                gyoztesplayer = btn_3.Text;
+                jatekvege();
+            }
+            
+        }
+        
+        public string jatekos()
+        {
+            switch (numberofclicks % 2 == 0)
+            {
+                case true:
+                    xorcircle = "X";
+                    break;
+                case false:
+                    xorcircle = "O";
+                    break;
+            }
+            numberofclicks++;
+            label1.Text = "Clicks: " + numberofclicks.ToString();
+            return xorcircle;
+        }
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            btn_1.Text = jatekos();
+            btn_1.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            btn_2.Text = jatekos();
+            btn_2.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            btn_3.Text = jatekos();
+            btn_3.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            btn_4.Text = jatekos();
+            btn_4.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            btn_5.Text = jatekos();
+            btn_5.Enabled = false;
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            btn_6.Text = jatekos();
+            btn_6.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            btn_7.Text = jatekos();
+            btn_7.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            btn_8.Text = jatekos();
+            btn_8.Enabled = false;
+            wincheck();
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            btn_9.Text = jatekos();
+            btn_9.Enabled = false;
+            wincheck();
+        }
+    }
+}
